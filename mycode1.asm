9ad7bd5d31565a82dc3a750b3d431d634cd60ee2
Harshi0906 <149461368+Harshi0906@users.noreply.github.com>
2024-08-13 20:22:07 +0530

    mycode1.asm

--- mycode1.asm
+++ mycode1.asm
@@ -1,8 +1,9 @@
- jmp start
-msg: db "Kasu Venkata Sai Harshita", 0Dh, 0Ah, 24h
-start: mov dx, msg
-mov ah, 09h
-int 21h
-mov ah, 0
-int 16h
-ret
+org 100h  
+jmp start  
+msg: db "Hello World", 0Dh, 0Ah, 24h  
+start: mov dx, msg  
+mov ah, 09h  
+int 21h  
+mov ah, 0  
+int 16h  
+ret 
